2b6d67fd7f6f9367549b53b11a738f6f259d8e2e
abduvali <97943192+normirzayev@users.noreply.github.com>
2023-04-10 16:28:27 +0500

    test

--- reactRouter/src/pages/Home.js
+++ reactRouter/src/pages/Home.js
@@ -1,21 +1,63 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
+import rasm1 from "../img/rasm1.jpg";
+import rasm2 from "../img/rasm2.jpeg";
+import rasm3 from "../img/rasm3.jpg";
+import rasm5 from "../img/rasm5.jpg";
+import rasm6 from "../img/rasm6.jpg";
+import rasm7 from "../img/rasm7.jpg";
+import rasm8 from "../img/rasm8.jpg";
+import rasm8_1 from "../img/rasm8.webp";
+import { Button } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 export default function Home() {
-  let text = useRef();
+  let rasmlar = [rasm1, rasm2, rasm3, rasm5, rasm6, rasm7, rasm8, rasm8_1];
+  let [sliderNumber, setSliderNumber] = useState(0);
 
-  useEffect(() => {
-    text.current.addEventListener("click", () => {
-      console.log("salom");
-    });
-  });
-
-  let handleChange = () => {
-    text.current.style.color = "red";
+  // sliderni orqaga o'tkazish uchun function
+  let handleSliderLast = () => {
+    setSliderNumber(sliderNumber - 1);
+    if (sliderNumber <= 0) {
+      setSliderNumber(rasmlar.length - 1);
+    }
+  };
+  // sliderni oldiga o'tkazish uchun function
+  let handleSliderNext = () => {
+    setSliderNumber(sliderNumber + 1);
+    if (sliderNumber >= rasmlar.length - 1) {
+      setSliderNumber(0);
+    }
   };
 
   return (
-    <div>
-      <h1 ref={text}>salom</h1>
-      <button onClick={handleChange}> change btn </button>
+    <div className="home">
+      <Button variant="contained" color="success" onClick={handleSliderLast}>
+        <ArrowBackIcon />
+      </Button>
+      <div
+        className="homeSlider"
+        style={{
+          gridTemplateColumns: `repeat(${rasmlar.length}, 100%)`,
+        }}
+      >
+        {rasmlar.map((rasm, index) => {
+          return (
+            <figure
+              key={index}
+              style={{
+                transform: `translate(calc(${sliderNumber}* -100%))`,
+                transition: "0.5s ease-in-out",
+                opacity: index === sliderNumber ? "1" : "0",
+              }}
+            >
+              <img src={rasm} />
+            </figure>
+          );
+        })}
+      </div>
+      <Button variant="contained" color="primary" onClick={handleSliderNext}>
+        <ArrowForwardIcon />
+      </Button>
     </div>
   );
 }
